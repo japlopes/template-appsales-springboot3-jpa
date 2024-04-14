1d56e2fb9ae6224d70965b26766ac68abc5f7f27
japlopes <developer55sx@gmail.com>
2024-04-14 18:53:21 -0300

    first commit - project created

---
+++ src/main/java/com/modelmono/appwholesale/AppWholesaleApplication.java
@@ -0,0 +1,13 @@
+package com.modelmono.appwholesale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWholesaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWholesaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/modelmono/appwholesale/AppWholesaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modelmono.appwholesale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWholesaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
