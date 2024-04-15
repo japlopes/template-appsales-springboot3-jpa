5f02936df45a005c654e01474892f13147d96961
japlopes <developer55sx@gmail.com>
2024-04-14 22:09:22 -0300

    sixth commit - Order, Instant, ISO 8601 - part 01

---
+++ src/main/java/com/modelmono/appwholesale/entities/Order.java
@@ -0,0 +1,81 @@
+package com.modelmono.appwholesale.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idOrder;
+	private Instant moment;
+	
+	@ManyToOne
+	@JoinColumn(name = "client_id")
+	private User client;
+
+	public Order() {
+	}
+
+	public Order(Long idOrder, Instant moment, User client) {
+		this.idOrder = idOrder;
+		this.moment = moment;
+		this.client = client;
+	}
+
+	public Long getIdOrder() {
+		return idOrder;
+	}
+
+	public void setIdOrder(Long idOrder) {
+		this.idOrder = idOrder;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
+		this.client = client;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(idOrder);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(idOrder, other.idOrder);
+	}
+	
+	
+
+}
