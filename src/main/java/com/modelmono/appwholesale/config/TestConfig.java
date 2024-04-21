a7ff235904b3e02f2def19582d51b2cd00ad95b0
japlopes <developer55sx@gmail.com>
2024-04-21 20:32:48 -0300

    twelfth commit - OrderItem, many-to-many association with extra attributes PART 1

--- src/main/java/com/modelmono/appwholesale/config/TestConfig.java
+++ src/main/java/com/modelmono/appwholesale/config/TestConfig.java
@@ -51,7 +51,6 @@ public class TestConfig implements CommandLineRunner {
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
 		p1.getCategories().add(cat2);
-		p2.getCategories().add(cat1);
 		p2.getCategories().add(cat2);
 		p3.getCategories().add(cat1);
 		p4.getCategories().add(cat3);
